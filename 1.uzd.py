7532d92d5cfd8786a2be73f96fd45e8966d52d48
Nikita <nikwok111@gmail.com>
2021-02-19 16:06:38 +0200

    Pievientos Main.py

--- 1.uzd.py
+++ 1.uzd.py
@@ -1,4 +1,18 @@
+#1.specifikācija
+cukurs_cena = float (cena_kg1*daudzums_kg1)
+kanelis_cena = float (cena_kg2*daudzums_kg2)
+aboli_daudzums_kg = float (daudzums_kg3) 
+udens_daudzums_kg = float (daudzums_kg4)
+summa = float (cukura_cena+kanelis_cena*aboli_daudzums)
+print("Summa:",summa)
 
+#2.specifikācija
+cukurs_cena = float (0.750.6)
+kanelis_cena = float (cena_kg2*daudzums_kg2)
+aboli_daudzums_kg = float (2) 
+udens_daudzums_kg = float (daudzums_kg4)
+summa = float (cukura_cena+kanelis_cena*aboli_daudzums)
+print("Summa:",summa)
 
 #3.specifikācija
 cukura_cena = float (0.75*0.6)
---
+++ 19.02.2021.py
@@ -0,0 +1,9 @@
+a = float (input())
+b = float (input())
+c = float (input())
+if b < a > c:
+    print (a)
+elif a < b > c:
+    print (b)
+elif a < c > b:
+    print (c)
